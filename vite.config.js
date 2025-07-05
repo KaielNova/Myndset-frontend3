0d10633243179394d5b98ec294e4702075bd50ef
KaielNova <factsonly32@gmail.com>
2025-07-05 15:59:37 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    strictPort: true,
+    port: 5173,
+    preview: {
+      port: 4173,
+      host: true,
+      allowedHosts: ['myndset-frontend3-production.up.railway.app']
+    }
   }
 })
