f01f4c523922ca18241b3bc0f611d8dc7c5f6e2e
KaielNova <factsonly32@gmail.com>
2025-07-11 13:24:32 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 import NovaDashboard from './components/NovaDashboard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <NovaDashboard />
     </div>
   );
