37ede70166b805b2621812d25eb7c70e059ae878
KaielNova <factsonly32@gmail.com>
2025-07-11 13:23:09 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import React from 'react';
 import NovaDashboard from './components/NovaDashboard';
-import DreamPortal from './components/DreamPortal';
-import LegacyU from './components/LegacyU';
-import ScrollHunt from './components/ScrollHunt';
-import WhatIfArena from './components/WhatIfArena';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/nova" element={<NovaDashboard />} />
-        <Route path="/dream" element={<DreamPortal />} />
-        <Route path="/legacyu" element={<LegacyU />} />
-        <Route path="/scrollhunt" element={<ScrollHunt />} />
-        <Route path="/whatif" element={<WhatIfArena />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <NovaDashboard />
+    </div>
   );
 }
 
